b266efd8acbc1870720968bb5f06a2179c6f32df
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2024-02-13 16:31:28 -0500

    fields validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const login = (req, res = response) => {
+
+    const errors = validationResult( req );
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
     res.json({
         ok: true,
         msg: 'login',
@@ -9,7 +19,16 @@ const login = (req, res = response) => {
 }
 
 const register = (req, res = response) => {
-    res.json({
+    
+    const errors = validationResult( req );
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+    
+    res.status(201).json({
         ok: true,
         msg: 'register',
         user: req.body
--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,27 @@
 
 const { Router } = require('express');
 const { login, register, renewToken } = require('../controllers/auth');
+const { check } = require('express-validator');
 const router = Router();
 
-router.post('/', login);
+router.post(
+    '/',
+    [
+        check('email', 'El correo es obligatorio.').isEmail(),
+        check('password', 'La contraseña debe contener al menos 6 caracteres.').isLength({ min: 6 })
+    ], 
+    login
+);
 
-router.post('/register', register);
+router.post(
+    '/register', 
+    [
+        check('name', 'El nombre es obligatorio.').not().isEmpty(),
+        check('email', 'El correo es obligatorio.').isEmail(),
+        check('password', 'La contraseña debe contener al menos 6 caracteres.').isLength({ min: 6 })
+    ],
+    register
+);
 
 router.get('/renewToken', renewToken);
 
