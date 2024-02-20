6c6373d4f1bcd56864214f74b74d0654df861496
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2024-02-19 23:51:30 -0500

    enable cors

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const {dbConnection} = require('./database/config');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
 
 const port = process.env.PORT;
 
 // Database
 dbConnection();
+
+// CORS
+app.use( cors() );
 
 // Public Dir
 app.use( express.static('public') );
